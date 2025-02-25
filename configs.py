c5876f1c4ae2f38fd5d9b2ba7b7ac9223c467fad
Devensh22345 <kumardevensh0@gmail.com>
2025-02-25 18:33:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,11 +3,11 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "22207976"))
     API_HASH = getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7586990531:AAFqwmiM6z88qAuNVyZ0nmCVzJQoXKhKGRg")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7728812955:AAH8KrAmU6fgaehoaW8diRUaKvDSPspSFCE")
     FSUB = getenv("FSUB", "Bot")
-    CHID = int(getenv("CHID", "-1002323780047"))
+    CHID = int(getenv("CHID", "-1002413846440"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Edit:Edit@cluster0.dzzd8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    LOG_CHANNEL = "-1002323780047"  # Replace with your log channel ID
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Auto:Auto@cluster0.illhz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    LOG_CHANNEL = "-1002413846440"  # Replace with your log channel ID
 
 cfg = Config()
