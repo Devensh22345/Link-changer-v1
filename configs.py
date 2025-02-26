12a37cb5f374823e96b32a3748eb2700394d7bb4
Devensh22345 <kumardevensh0@gmail.com>
2025-02-26 11:27:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "8112564584:AAFTqWtQ4L1uezP6-SDAG5YE27pdQO4w2iI")
     FSUB = getenv("FSUB", "Bot")
     CHID = int(getenv("CHID", "-1002413846440"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("SUDO", "6872968794").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Auto1:Auto1@cluster0.umt5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOG_CHANNEL = "-1002413846440"  # Replace with your log channel ID
 
