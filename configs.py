7d15b6f0cdb89544bf3422185f43e0a26398d3f8
Devensh22345 <kumardevensh0@gmail.com>
2025-02-26 20:53:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,11 @@
-from os import path, getenv
+from os import getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
-    
+    API_ID = int(getenv("API_ID", "22207976"))  
+    API_HASH = getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")  
+    BOT_TOKEN = getenv("BOT_TOKEN", "8017555909:AAHkb7UMF8GNzYZcrO9shconqPwlfE73qe0")  
+    SESSION_STRING = getenv("SESSION_STRING", "")  
+    SUDO = list(map(int, getenv("SUDO", "6872968794").split()))  
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Test:Test@cluster0.pcpx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  
+
 cfg = Config()
