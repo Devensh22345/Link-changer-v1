0f081fd371748990234aeb9d41b9c0c28d69e79f
Devensh22345 <kumardevensh0@gmail.com>
2025-03-01 22:16:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,47 +1,56 @@
 from pymongo import MongoClient
 from configs import cfg
 
+# Initialize MongoDB Client
 client = MongoClient(cfg.MONGO_URI)
 
-users = client['main']['users']
-groups = client['main']['groups']
+# Database and Collections
+db = client['main']
+users = db['users']  # Sudo users
+channels = db['channels']  # Created channels
 
-def already_db(user_id):
-        user = users.find_one({"user_id" : str(user_id)})
-        if not user:
-            return False
-        return True
+# SUDO USER FUNCTIONS
 
-def already_dbg(chat_id):
-        group = groups.find_one({"chat_id" : str(chat_id)})
-        if not group:
-            return False
-        return True
+def is_sudo_user(user_id: int) -> bool:
+    """Check if a user is a sudo user."""
+    return users.find_one({"user_id": user_id}) is not None
 
-def add_user(user_id):
-    in_db = already_db(user_id)
-    if in_db:
-        return
-    return users.insert_one({"user_id": str(user_id)}) 
+def add_sudo_user(user_id: int) -> None:
+    """Add a user to the sudo user list."""
+    if not is_sudo_user(user_id):
+        users.insert_one({"user_id": user_id})
 
-def remove_user(user_id):
-    in_db = already_db(user_id)
-    if not in_db:
-        return 
-    return users.delete_one({"user_id": str(user_id)})
-    
-def add_group(chat_id):
-    in_db = already_dbg(chat_id)
-    if in_db:
-        return
-    return groups.insert_one({"chat_id": str(chat_id)})
+def remove_sudo_user(user_id: int) -> None:
+    """Remove a user from the sudo user list."""
+    users.delete_one({"user_id": user_id})
 
-def all_users():
-    user = users.find({})
-    usrs = len(list(user))
-    return usrs
+def get_all_sudo_users() -> list:
+    """Return a list of all sudo user IDs."""
+    return [user["user_id"] for user in users.find()]
 
-def all_groups():
-    group = groups.find({})
-    grps = len(list(group))
-    return grps
+def total_sudo_users() -> int:
+    """Return the total count of sudo users."""
+    return users.count_documents({})
+
+# CHANNEL FUNCTIONS
+
+def add_channel(channel_id: int, channel_name: str, created_by: int) -> None:
+    """Add a created channel to the database."""
+    if not channels.find_one({"channel_id": channel_id}):
+        channels.insert_one({
+            "channel_id": channel_id,
+            "channel_name": channel_name,
+            "created_by": created_by
+        })
+
+def is_channel_in_db(channel_id: int) -> bool:
+    """Check if a channel exists in the database."""
+    return channels.find_one({"channel_id": channel_id}) is not None
+
+def get_all_channels() -> list:
+    """Return a list of all created channels."""
+    return list(channels.find({}))
+
+def total_channels() -> int:
+    """Return the total count of created channels."""
+    return channels.count_documents({})
