d066e9ab7578318afc5c5b69eb7a0422be32e358
Devensh22345 <kumardevensh0@gmail.com>
2025-03-03 00:41:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,9 +82,11 @@ async def start(_, m: Message):
                     [InlineKeyboardButton("𝐀𝐧𝐢𝐦𝐞 𝐢𝐧 𝐇𝐢𝐧𝐝𝐢", url="https://t.me/+2fsV4nzHvOs2OGNl")],
                     [InlineKeyboardButton("𝐒𝐨𝐥𝐨 𝐋𝐞𝐯𝐞𝐥𝐢𝐧𝐠", url="https://t.me/+BYFsBvSb8eM5ZTc1")],
                     [InlineKeyboardButton("𝐍𝐚𝐫𝐮𝐭𝐨 𝐬𝐡𝐢𝐩𝐩𝐮𝐝𝐞𝐧", url="https://t.me/+1Uqfi_EB69s3MDVl")],
+                    [InlineKeyboardButton("𝐒𝐚𝐤𝐚𝐦𝐨𝐭𝐨 𝐃𝐚𝐲𝐬", url="https://t.me/+KwzdqOCMeVVhMjJl")],
                     [InlineKeyboardButton("𝐃𝐞𝐦𝐨𝐧 𝐬𝐥𝐚𝐲𝐞𝐫", url="https://t.me/+-Uh3oEL5NKBjMzZl")],
                     [InlineKeyboardButton("𝐀𝐭𝐭𝐚𝐜𝐤 𝐨𝐧 𝐓𝐢𝐭𝐚𝐧", url="https://t.me/+bxKksmx6D5I5ZmNl")],
-                    [InlineKeyboardButton("𝐃𝐞𝐚𝐭𝐡 𝐧𝐨𝐭𝐞", url="https://t.me/+llU_niDFQxwxYTJl")]
+                    [InlineKeyboardButton("𝐉𝐮𝐣𝐮𝐭𝐬𝐮 𝐤𝐚𝐢𝐬𝐞𝐧", url="https://t.me/+ItHYxazxuI1lOWZl")],
+                    [InlineKeyboardButton("𝐃𝐞𝐚𝐭𝐡 𝐧𝐨𝐭𝐞", url="https://t.me/+z3ZvLLx1ZRUwYzRl")]
                 ]
             )
 
