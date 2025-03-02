5ffd1952be951c917066bcaaaa3d779643234e19
Devensh22345 <kumardevensh0@gmail.com>
2025-03-03 01:36:12 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,45 +3,72 @@ from configs import cfg
 
 client = MongoClient(cfg.MONGO_URI)
 
-users = client['main']['users']
-groups = client['main']['groups']
+# Databases and Collections
+db = client['main']
+users = db['users']
+groups = db['groups']
+sudo_users = db['sudo_users']
+authorized_users = db['authorized_users']
 
+# Check if User Exists in DB
 def already_db(user_id):
-        user = users.find_one({"user_id" : str(user_id)})
-        if not user:
-            return False
-        return True
+    return users.find_one({"user_id": str(user_id)}) is not None
 
+# Check if Group Exists in DB
 def already_dbg(chat_id):
-        group = groups.find_one({"chat_id" : str(chat_id)})
-        if not group:
-            return False
-        return True
+    return groups.find_one({"chat_id": str(chat_id)}) is not None
 
+# Add User to DB
 def add_user(user_id):
-    in_db = already_db(user_id)
-    if in_db:
-        return
-    return users.insert_one({"user_id": str(user_id)}) 
+    if not already_db(user_id):
+        users.insert_one({"user_id": str(user_id)})
 
+# Remove User from DB
 def remove_user(user_id):
-    in_db = already_db(user_id)
-    if not in_db:
-        return 
-    return users.delete_one({"user_id": str(user_id)})
-    
+    if already_db(user_id):
+        users.delete_one({"user_id": str(user_id)})
+
+# Add Group to DB
 def add_group(chat_id):
-    in_db = already_dbg(chat_id)
-    if in_db:
-        return
-    return groups.insert_one({"chat_id": str(chat_id)})
+    if not already_dbg(chat_id):
+        groups.insert_one({"chat_id": str(chat_id)})
 
+# Get Total Number of Users
 def all_users():
-    user = users.find({})
-    usrs = len(list(user))
-    return usrs
+    return len(list(users.find({})))
 
+# Get Total Number of Groups
 def all_groups():
-    group = groups.find({})
-    grps = len(list(group))
-    return grps
+    return len(list(groups.find({})))
+
+# Add Sudo User
+def add_sudo_user(user_id):
+    if not is_sudo_user(user_id):
+        sudo_users.insert_one({"user_id": str(user_id)})
+
+# Remove Sudo User
+def remove_sudo_user(user_id):
+    if is_sudo_user(user_id):
+        sudo_users.delete_one({"user_id": str(user_id)})
+
+# Check if User is a Sudo User
+def is_sudo_user(user_id):
+    return sudo_users.find_one({"user_id": str(user_id)}) is not None
+
+# Get All Sudo Users
+def get_sudo_users():
+    return [int(user['user_id']) for user in sudo_users.find()]
+
+# Authorize User to Avoid Deletion
+def authorize_user(user_id):
+    if not is_authorized_user(user_id):
+        authorized_users.insert_one({"user_id": str(user_id)})
+
+# Unauthorize User to Revoke Permission
+def unauthorize_user(user_id):
+    if is_authorized_user(user_id):
+        authorized_users.delete_one({"user_id": str(user_id)})
+
+# Check if User is Authorized
+def is_authorized_user(user_id):
+    return authorized_users.find_one({"user_id": str(user_id)}) is not None
