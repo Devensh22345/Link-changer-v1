39b8997d92b068668e4797cb0771f234088672da
Devensh22345 <kumardevensh0@gmail.com>
2025-03-03 02:22:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,72 +3,82 @@ from configs import cfg
 
 client = MongoClient(cfg.MONGO_URI)
 
-# Databases and Collections
-db = client['main']
-users = db['users']
-groups = db['groups']
-sudo_users = db['sudo_users']
-authorized_users = db['authorized_users']
+users = client['main']['users']
+groups = client['main']['groups']
+auths = client['main']['auths']
+sudos = client['main']['sudos']
 
-# Check if User Exists in DB
+# Check if user exists in the database
 def already_db(user_id):
-    return users.find_one({"user_id": str(user_id)}) is not None
+    return bool(users.find_one({"user_id": str(user_id)}))
 
-# Check if Group Exists in DB
+# Check if group exists in the database
 def already_dbg(chat_id):
-    return groups.find_one({"chat_id": str(chat_id)}) is not None
+    return bool(groups.find_one({"chat_id": str(chat_id)}))
 
-# Add User to DB
+# Add a new user to the database
 def add_user(user_id):
     if not already_db(user_id):
         users.insert_one({"user_id": str(user_id)})
 
-# Remove User from DB
+# Remove a user from the database
 def remove_user(user_id):
     if already_db(user_id):
         users.delete_one({"user_id": str(user_id)})
 
-# Add Group to DB
+# Add a new group to the database
 def add_group(chat_id):
     if not already_dbg(chat_id):
         groups.insert_one({"chat_id": str(chat_id)})
 
-# Get Total Number of Users
+# Get the total number of users
 def all_users():
-    return len(list(users.find({})))
+    return users.count_documents({})
 
-# Get Total Number of Groups
+# Get the total number of groups
 def all_groups():
-    return len(list(groups.find({})))
+    return groups.count_documents({})
 
-# Add Sudo User
-def add_sudo_user(user_id):
-    if not is_sudo_user(user_id):
-        sudo_users.insert_one({"user_id": str(user_id)})
+# Get all authorized users in a group
+def get_auth(chat_id):
+    auth_data = auths.find_one({"chat_id": str(chat_id)})
+    return auth_data['auth_users'] if auth_data else []
 
-# Remove Sudo User
-def remove_sudo_user(user_id):
-    if is_sudo_user(user_id):
-        sudo_users.delete_one({"user_id": str(user_id)})
+# Add an authorized user to a group
+def add_auth(chat_id, user_id):
+    auth_data = auths.find_one({"chat_id": str(chat_id)})
+    if auth_data:
+        if str(user_id) not in auth_data['auth_users']:
+            auth_data['auth_users'].append(str(user_id))
+            auths.update_one({"chat_id": str(chat_id)}, {"$set": {"auth_users": auth_data['auth_users']}})
+    else:
+        auths.insert_one({"chat_id": str(chat_id), "auth_users": [str(user_id)]})
 
-# Check if User is a Sudo User
-def is_sudo_user(user_id):
-    return sudo_users.find_one({"user_id": str(user_id)}) is not None
+# Remove an authorized user from a group
+def remove_auth(chat_id, user_id):
+    auth_data = auths.find_one({"chat_id": str(chat_id)})
+    if auth_data and str(user_id) in auth_data['auth_users']:
+        auth_data['auth_users'].remove(str(user_id))
+        auths.update_one({"chat_id": str(chat_id)}, {"$set": {"auth_users": auth_data['auth_users']}})
 
-# Get All Sudo Users
-def get_sudo_users():
-    return [int(user['user_id']) for user in sudo_users.find()]
+# Get all sudo users
+def get_sudo():
+    sudo_data = sudos.find_one({"sudo": "users"})
+    return sudo_data['sudo_users'] if sudo_data else []
 
-# Authorize User to Avoid Deletion
-def authorize_user(user_id):
-    if not is_authorized_user(user_id):
-        authorized_users.insert_one({"user_id": str(user_id)})
+# Add a sudo user
+def add_sudo(user_id):
+    sudo_data = sudos.find_one({"sudo": "users"})
+    if sudo_data:
+        if str(user_id) not in sudo_data['sudo_users']:
+            sudo_data['sudo_users'].append(str(user_id))
+            sudos.update_one({"sudo": "users"}, {"$set": {"sudo_users": sudo_data['sudo_users']}})
+    else:
+        sudos.insert_one({"sudo": "users", "sudo_users": [str(user_id)]})
 
-# Unauthorize User to Revoke Permission
-def unauthorize_user(user_id):
-    if is_authorized_user(user_id):
-        authorized_users.delete_one({"user_id": str(user_id)})
-
-# Check if User is Authorized
-def is_authorized_user(user_id):
-    return authorized_users.find_one({"user_id": str(user_id)}) is not None
+# Remove a sudo user
+def remove_sudo(user_id):
+    sudo_data = sudos.find_one({"sudo": "users"})
+    if sudo_data and str(user_id) in sudo_data['sudo_users']:
+        sudo_data['sudo_users'].remove(str(user_id))
+        sudos.update_one({"sudo": "users"}, {"$set": {"sudo_users": sudo_data['sudo_users']}})
