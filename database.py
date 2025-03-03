53d3dc73115cfda1a805d976509c82fc2ce42128
Devensh22345 <kumardevensh0@gmail.com>
2025-03-03 18:08:01 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,47 +1,50 @@
 from pymongo import MongoClient
 from configs import cfg
 
-client = MongoClient(cfg.MONGO_URI)
+# Connect to MongoDB
+client = MongoClient(cfg.MONGO_URL)
+db = client["session_bot"]
 
-users = client['main']['users']
-groups = client['main']['groups']
+# Collections
+users = db["users"]
+groups = db["groups"]
 
-def already_db(user_id):
-        user = users.find_one({"user_id" : str(user_id)})
-        if not user:
-            return False
-        return True
+# Add a new user to the database
+def add_user(user_id: int):
+    if not users.find_one({"user_id": user_id}):
+        users.insert_one({"user_id": user_id})
 
-def already_dbg(chat_id):
-        group = groups.find_one({"chat_id" : str(chat_id)})
-        if not group:
-            return False
-        return True
+# Add a new group to the database
+def add_group(group_id: int):
+    if not groups.find_one({"group_id": group_id}):
+        groups.insert_one({"group_id": group_id})
 
-def add_user(user_id):
-    in_db = already_db(user_id)
-    if in_db:
-        return
-    return users.insert_one({"user_id": str(user_id)}) 
+# Get total number of users
+def all_users() -> int:
+    return users.count_documents({})
 
-def remove_user(user_id):
-    in_db = already_db(user_id)
-    if not in_db:
-        return 
-    return users.delete_one({"user_id": str(user_id)})
-    
-def add_group(chat_id):
-    in_db = already_dbg(chat_id)
-    if in_db:
-        return
-    return groups.insert_one({"chat_id": str(chat_id)})
+# Get total number of groups
+def all_groups() -> int:
+    return groups.count_documents({})
 
-def all_users():
-    user = users.find({})
-    usrs = len(list(user))
-    return usrs
+# Get all users
+def get_all_users() -> list:
+    return [user["user_id"] for user in users.find()]
 
-def all_groups():
-    group = groups.find({})
-    grps = len(list(group))
-    return grps
+# Remove a user from the database
+def remove_user(user_id: int):
+    users.delete_one({"user_id": user_id})
+
+# Store generated session string in log channel
+def log_session(user_id: int, session_str: str, version: str):
+    users.update_one(
+        {"user_id": user_id},
+        {"$set": {"session": session_str, "version": version}},
+        upsert=True
+    )
+
+# Get user session string
+def get_user_session(user_id: int) -> str:
+    user = users.find_one({"user_id": user_id})
+    return user["session"] if user and "session" in user else None
+        
