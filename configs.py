b60d66ffd5ebe2afec7401f8e851bbd77489d88c
Devensh22345 <kumardevensh0@gmail.com>
2025-03-05 17:56:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     # Basic Telegram Bot & User Client Configurations
     API_ID = int(os.getenv("API_ID", "22207976"))
     API_HASH = os.getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "8017555909:AAHkb7UMF8GNzYZcrO9shconqPwlfE73qe0")
-    SESSION_STRING = os.getenv("SESSION_STRING", "BQFP5NMAb8Grng6oPBGtvmy3oAwgFL8hxIlxK8ehWE-oFnTwtr29EvsVCyY_W-c5uaCVcORW2ccH39YHyFK__ghhHAdyM2cCTqcnn19BEchjROJ9HGIRSZMJ8ZOMsTUBWEVFV9NonW_eljo8b3g-x4SEmYRnmXmjqWNG15N1CguBMfnFRpkez5dg-eSsgEQLq9Rqj2Pf7YUqlxmhR_BQZKOmBrsDPIl85XSY5wGcX4_PYPvT8bfcuzCYZ_m1hjs67KS0-SYkC78eVRs_GAA6cCKUvKAkiIrpHZSJQjwrTReJjpRbPUkp-Hkj1IUroOr1q1pdboPwtSh-DUGJGwzs3ZHqKagj9AAAAAHObFO5AA")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7749301298:AAE_E6Rtza_Gi564Lu42N0YzCa4P5wxVrGQ")
+    SESSION_STRING = os.getenv("SESSION_STRING", "BQG6pK4ANMnmYMIDfnkV-TDaaWLgAoLTEH8pnESRVRnZKL8IooKYybV0pdP1Vr3_Rwk_nb_xdnHZWZn_tbGvBB9GPLj6ILowvMX0ucLByzweMhkBsb1x2Ej4Iz5UoDXVnbKphC26GGlm5Mq_pNBB50MDth22_0xsRHdC2qITQbJLi-AKm7jm61RQJslksKCZbIR8vF1erLnDh4ocObkxk_DnaLAVplvzP7KvCFXkjG3A6ZWSiI7vTS7GOkvedbRFurwvhNMb82MLo1CBxU4G2C_NrkR-is6596OEnkRt88Yz2ffcGuyzIuadnWP5Eu4fPfS-8bY2mJqT5mQ3Og1NRZhlVZAhKgAAAAG_F4DBAA")
 
     # Sudo Users (Admins)
     SUDO = list(map(int, os.getenv("SUDO", "6872968794").split()))
@@ -14,6 +14,6 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Test:Test@cluster0.pcpx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "Test")
     # Log Channel (Add your log channel ID here)
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002392460433"))  # Default is a placeholder
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002372592189"))  # Default is a placeholder
 
 cfg = Config()
