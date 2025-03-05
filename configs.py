860201d944914ce0d8107033b065f9d0b67efedc
Devensh22345 <kumardevensh0@gmail.com>
2025-03-05 16:12:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,6 @@ class Config:
     API_ID = int(os.getenv("API_ID", "22207976"))
     API_HASH = os.getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "8017555909:AAHkb7UMF8GNzYZcrO9shconqPwlfE73qe0")
-    SESSION_STRING = os.getenv("SESSION_STRING", "BQFP5NMAb8Grng6oPBGtvmy3oAwgFL8hxIlxK8ehWE-oFnTwtr29EvsVCyY_W-c5uaCVcORW2ccH39YHyFK__ghhHAdyM2cCTqcnn19BEchjROJ9HGIRSZMJ8ZOMsTUBWEVFV9NonW_eljo8b3g-x4SEmYRnmXmjqWNG15N1CguBMfnFRpkez5dg-eSsgEQLq9Rqj2Pf7YUqlxmhR_BQZKOmBrsDPIl85XSY5wGcX4_PYPvT8bfcuzCYZ_m1hjs67KS0-SYkC78eVRs_GAA6cCKUvKAkiIrpHZSJQjwrTReJjpRbPUkp-Hkj1IUroOr1q1pdboPwtSh-DUGJGwzs3ZHqKagj9AAAAAHObFO5AA")
 
     # Sudo Users (Admins)
     SUDO = list(map(int, os.getenv("SUDO", "6872968794").split()))
@@ -13,7 +12,13 @@ class Config:
     # MongoDB Configuration
     MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Test:Test@cluster0.pcpx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "Test")
-    # Log Channel (Add your log channel ID here)
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002392460433"))  # Default is a placeholder
+
+    # Log Channel (for storing logs and session data)
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002392460433"))
+
+    # Default Values for Creating Channels and Temporary Channels
+    TEMP_CHANNEL_PREFIX = os.getenv("TEMP_CHANNEL_PREFIX", "Temp-")
+    CHANNEL_CREATION_TIMEOUT = int(os.getenv("CHANNEL_CREATION_TIMEOUT", "18000"))  # 5 hours (in seconds)
+    USERNAME_CHANGE_INTERVAL = int(os.getenv("USERNAME_CHANGE_INTERVAL", "3600"))  # 1 hour (in seconds)
 
 cfg = Config()
