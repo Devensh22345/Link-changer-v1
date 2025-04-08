574576390e9032cc5d024bf5d57c99f855f7ef93
Devensh22345 <kumardevensh0@gmail.com>
2025-04-09 04:55:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,8 +5,14 @@ class Config:
     API_ID = int(os.getenv("API_ID", "22207976"))
     API_HASH = os.getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "7686232626:AAFb8LoDG_Ioy-r3pGA9gfJejRm4I60HCnA")
-    SESSION_STRING = os.getenv("SESSION_STRING", "BQGofiMAE-6C5Fa73QyvAw7oiLskI59RpgP1fEXijpWofwc39WV71-PzHd1mFGr7uWeDFaajV-eHIDIA48DvIdb4aZjym8bw3PTcHGXVCMAy4oTubpnC8DBzINo_xtpvEZhRUourv59mupimDuIJUX6fwAc-rRVDDsWKxLDTAz1-3D0cXL39CVScpSOUoURx-s9J3tJtsma0H3TYVX_ArNVfj0jfzZt40O-YC85eO6SXyDbdz2rQNfu2utoRAqeDPwJNvb1UbAqDEWUFRqoAuhPSAxUwiRuITR-yJ7veJs1-qnuST67i-OEicHWSA4c3zDxlDWT2HmkFSBZzX7QowIDM4IV1QQAAAAHLm2QHAA")
-
+    
+    # Support for multiple session strings: SESSION1, SESSION2, SESSION3, etc.
+    SESSIONS = {
+        key.lower(): value
+        for key, value in os.environ.items()
+        if key.startswith("SESSION")
+    } 
+    
     # Sudo Users (Admins)
     SUDO = list(map(int, os.getenv("SUDO", "6872968794").split()))
 
